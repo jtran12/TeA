95d8a34304710470c66cbffee1ea955e8daefd32
tony-h-lee <tonyhjlee@gmail.com>
2017-03-11 19:28:16 -0500

    Mock state into courselist and added courselist single component

--- client/src/components/course/CourseListSingle.jsx
+++ client/src/components/course/CourseListSingle.jsx
@@ -7,28 +7,15 @@ class CourseList extends React.Component {
     super(props);
 
     this.state = {
-      courses: [
-        {
-          courseCode: "CSC108"
-        },
-        {
-          courseCode: "CSC148"
-        },
-        {
-          courseCode: "CSC165"
-        }
-      ]
     };
   }
 
   render() {
     const styles = lodash.cloneDeep(this.constructor.styles);
 
-    const courses = this.state.courses.map((course) => <div> { course.courseCode } </div> );
-
     return (
       <div>
-        {courses}
+        <h3>Course List</h3>
       </div>
     );
   }
