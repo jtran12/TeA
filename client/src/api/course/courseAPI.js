b669e45b9fc39ed32120a1fddd368e864409f268
tony-h-lee <tonyhjlee@gmail.com>
2017-03-18 18:41:17 -0400

    added warning into course fetch

--- client/src/api/course/courseAPI.js
+++ client/src/api/course/courseAPI.js
@@ -3,7 +3,10 @@ import fetch from 'isomorphic-fetch';
 class CourseAPI {
 
   static getCourses() {
+
+    // REPLACE COURSE_MOCK_URL WITH SERVER API URL ONCE IT'S THERE !!!
     const COURSE_MOCK_URL = './mock/course/course.json';
+
     return fetch(COURSE_MOCK_URL).then(response => {
       return response.json();
     }).catch(error => {
