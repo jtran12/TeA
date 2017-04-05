2dd1016c644f691a9012614573853364742e4a9b
tony-h-lee <tonyhjlee@gmail.com>
2017-04-05 14:49:08 -0400

    assing applicant to course actions

--- client/src/api/applicant/applicantAPI.js
+++ client/src/api/applicant/applicantAPI.js
@@ -3,10 +3,25 @@ import fetch from 'isomorphic-fetch';
 class ApplicantAPI {
 
   static getApplicants(currSize) {
-
     const APPLICANT_API = `http://localhost:3000/applicant/all?limit=10&offset=${currSize}`;
-
     return fetch(APPLICANT_API)
+      .then(response => response.json())
+      .catch(error => error);
+  }
+
+  static assignApplicantToCourse(course) {
+    const init = {
+      method: 'POST',
+      body: JSON.stringify({
+        course: course.course,
+        coursecode: course.corsecode,
+        term: course.term,
+        year: course.year,
+        utorid: course.utorid
+      })
+    }
+    const APPLICANT_API = `http://localhost:3000/offer`;
+    return fetch(APPLICANT_API, init)
       .then(response => response.json())
       .catch(error => error);
   }
