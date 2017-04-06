09c423f0308eb8a5643f7279f0e39121316f7e25
Jason Tran <jasontran1209@gmail.com>
2017-04-06 15:31:14 -0400

    Changed course modal to reflect all applicants in the database

--- client/src/components/applicant/applicanttable/ApplicantTable.jsx
+++ client/src/components/applicant/applicanttable/ApplicantTable.jsx
@@ -9,89 +9,65 @@ import RaisedButton from 'material-ui/RaisedButton';
 import LinearProgress from 'material-ui/LinearProgress';
 import Dialog from 'material-ui/Dialog';
 
+import { connect } from 'react-redux';
+import { Scrollbars } from 'react-custom-scrollbars';
+import * as applicantActions from '../../../actions/applicant/applicantActions';
+
 import AddIcon from 'material-ui/svg-icons/content/add';
 import RemoveIcon from 'material-ui/svg-icons/content/clear';
 import CourseCoordinatorIcon from 'material-ui/svg-icons/social/person-outline';
+
+
 
 class ApplicantTable extends React.Component {
 
   constructor(props){
     super(props);
 
-    this.state = {
-    };
   }
+
+  componentDidMount() {
+    this.props.loadApplicants([]);
+  }
+
 
   render() {
     const styles = lodash.cloneDeep(this.constructor.styles);
+    const applicants = this.props.applicant.applicants;
 
     return (
       <div>
         <h3>Applicants</h3>
-          <Table fixedHeader={true}>
+        <Scrollbars style={{ width: 700, height: 400 }}>
+          <Table fixedHeader={false}>
             <TableHeader displaySelectAll={false} adjustForCheckbox={false} sortable={true}>
               <TableRow>
-                <TableHeaderColumn> <RaisedButton primary={true} bsSize="xsmall">+ Graduate Only</RaisedButton> </TableHeaderColumn>
-                <TableHeaderColumn> <RaisedButton primary={true} bsSize="xsmall">+ Previous Experience</RaisedButton> </TableHeaderColumn>
-                <TableHeaderColumn> <RaisedButton primary={true} bsSize="xsmall">+ Recommended</RaisedButton> </TableHeaderColumn>
-              </TableRow>
-              <TableRow>
-                <TableHeaderColumn style={styles.idwidth}>ID</TableHeaderColumn>
-                <TableHeaderColumn>First Name</TableHeaderColumn>
-                <TableHeaderColumn>Last Name</TableHeaderColumn>
+                <TableHeaderColumn style={styles.idwidth}>Utorid</TableHeaderColumn>
+                <TableHeaderColumn>Name</TableHeaderColumn>
                 <TableHeaderColumn>Enrollment Level</TableHeaderColumn>
+                <TableHeaderColumn>Year</TableHeaderColumn>
                 <TableHeaderColumn />
               </TableRow>
             </TableHeader>
             {/* The table headers should allow sortable which will be included in a function later */}
-            <TableBody displayRowCheckbox={false} showRowHover={true} stripedRows={false}>
-              <TableRow selectable={false}>
-                <TableRowColumn style={styles.idwidth}>1001143223</TableRowColumn>
-                <TableRowColumn>Test</TableRowColumn>
-                <TableRowColumn>User</TableRowColumn>
-                <TableRowColumn>UG - 2</TableRowColumn>
-                <TableRowColumn>
-                  <RaisedButton primary={true} icon={<AddIcon/>}/>
-                </TableRowColumn>
-              </TableRow>
-              <TableRow selectable={false}>
-                <TableRowColumn style={styles.idwidth}>1001143223</TableRowColumn>
-                <TableRowColumn>Tester</TableRowColumn>
-                <TableRowColumn>User</TableRowColumn>
-                <TableRowColumn>UG - 4</TableRowColumn>
-                <TableRowColumn>
-                  <RaisedButton primary={true} icon={<AddIcon/>}/>
-                </TableRowColumn>
-              </TableRow>
-              <TableRow selectable={false}>
-                <TableRowColumn style={styles.idwidth}>1001143223</TableRowColumn>
-                <TableRowColumn>PhdTest</TableRowColumn>
-                <TableRowColumn>User</TableRowColumn>
-                <TableRowColumn>Graduate - 2</TableRowColumn>
-                <TableRowColumn>
-                  <RaisedButton primary={true} icon={<AddIcon/>}/>
-                </TableRowColumn>
-              </TableRow>
-              <TableRow selectable={false}>
-                <TableRowColumn style={styles.idwidth}>1001143223</TableRowColumn>
-                <TableRowColumn>PhDMan</TableRowColumn>
-                <TableRowColumn>User</TableRowColumn>
-                <TableRowColumn>Graduate - 1</TableRowColumn>
-                <TableRowColumn>
-                  <RaisedButton primary={true} icon={<AddIcon/>}/>
-                </TableRowColumn>
-              </TableRow>
-              <TableRow selectable={false}>
-                <TableRowColumn style={styles.idwidth}>1001143223</TableRowColumn>
-                <TableRowColumn>MastersMan</TableRowColumn>
-                <TableRowColumn>User</TableRowColumn>
-                <TableRowColumn>Graduate - 1</TableRowColumn>
-                <TableRowColumn>
-                  <RaisedButton primary={true} icon={<AddIcon/>}/>
-                </TableRowColumn>
-              </TableRow>
-            </TableBody>
+            
+              <TableBody displayRowCheckbox={false} showRowHover={true} stripedRows={false}>
+               {
+                  applicants.map((applicant, index) =>  (
+                    <TableRow selectable={false} key={index}>
+                      <TableRowColumn style={styles.idwidth} >{applicant.utorid}</TableRowColumn>
+                      <TableRowColumn>{applicant.familyname} {applicant.givenname}</TableRowColumn>
+                      <TableRowColumn>{applicant.program.toUpperCase()}</TableRowColumn>
+                      <TableRowColumn>{applicant.year}</TableRowColumn>
+                      <TableRowColumn>
+                        <RaisedButton primary={true} icon={<AddIcon/>}/>
+                      </TableRowColumn>
+                    </TableRow>
+                  ))
+               }
+              </TableBody>
           </Table>
+          </Scrollbars>
       </div>
     );
   }
@@ -101,4 +77,17 @@ ApplicantTable.styles = {
 
 };
 
-export default ApplicantTable;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    applicant: state.applicant
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadApplicants: (curr) => dispatch(applicantActions.loadApplicants(curr)),
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ApplicantTable);
+
