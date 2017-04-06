e5c942ca0dac45eae8617c05d0301f42ffc06240
Jason Tran <jasontran1209@gmail.com>
2017-04-06 18:11:37 -0400

    Applicant table properly displays the utorid and recommendation score (#88)

--- client/src/components/applicant/applicantrecommendations/ApplicantRecommendations.jsx
+++ client/src/components/applicant/applicantrecommendations/ApplicantRecommendations.jsx
@@ -8,7 +8,10 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Menu from 'material-ui/svg-icons/navigation/menu';
 
+import { connect } from 'react-redux';
 import { Scrollbars } from 'react-custom-scrollbars';
+import * as courseActions from '../../../actions/course/courseActions';
+
 
 @Radium
 class ApplicantRecommendations extends React.Component {
@@ -17,33 +20,7 @@ class ApplicantRecommendations extends React.Component {
     super(props);
 
     this.state = {
-      recommendedApplicants: [
-        {
-          id: '1001143223',
-          firstName: 'David',
-          lastName: 'Davis',
-          studentLevel: 'Year 1 Graduate Student'
-        },
-        {
-          id: '1054367243',
-          firstName: 'Lisa',
-          lastName: 'Elissa',
-          studentLevel: 'Year 2 Undergraduate Student'
-        },
-        {
-          id: '1436345454',
-          firstName: 'Michael',
-          lastName: 'Carmichael',
-          studentLevel: 'Year 4 Undergraduate Student'
-        },
-        {
-          id: '2445345454',
-          firstName: 'Jenny',
-          lastName: 'Janice',
-          studentLevel: 'Year 1 Undergraduate Student'
-        }
-      ]
-    };
+    }
   }
 
 
@@ -51,31 +28,37 @@ class ApplicantRecommendations extends React.Component {
   render() {
     const styles = lodash.cloneDeep(this.constructor.styles);
 
+    const course = this.props.courses.selectedCourse;
+
     return (
       <div className="recommendations">
         <Scrollbars style={{ width: 600, height: 400 }}>
           <Table fixedHeader={false}>
             <TableHeader displaySelectAll={false} adjustForCheckbox={false} sortable={true}>
               <TableRow>
-                <TableHeaderColumn style={styles.idwidth}>Utorid</TableHeaderColumn>
-                <TableHeaderColumn>Name</TableHeaderColumn>
-                <TableHeaderColumn>Enrollment Level</TableHeaderColumn>
-                <TableHeaderColumn>Year</TableHeaderColumn>
+                <TableHeaderColumn style={styles.idwidth} tooltip="Unique ID for each student">
+                Utorid</TableHeaderColumn>
+                <TableHeaderColumn tooltip="Score is calculated based on our recommendation system">
+                Recommendation Score</TableHeaderColumn>
                 <TableHeaderColumn />
               </TableRow>
             </TableHeader>
             {/* The table headers should allow sortable which will be included in a function later */}
             
               <TableBody displayRowCheckbox={false} showRowHover={true} stripedRows={false}>
-                    <TableRow selectable={false}>
-                      <TableRowColumn style={styles.idwidth} >testID</TableRowColumn>
-                      <TableRowColumn>User Name</TableRowColumn>
-                      <TableRowColumn>Program name</TableRowColumn>
-                      <TableRowColumn>Program year</TableRowColumn>
+               {
+                  course.recommended_applicants.map((applicant, index) =>  (
+                    <TableRow selectable={false} key={index}>
+                      <TableRowColumn style={styles.idwidth} >
+                      {applicant.slice(0, applicant.indexOf(" "))}</TableRowColumn>
+                      <TableRowColumn style={styles.idwidth} >
+                      {applicant.slice(applicant.indexOf(" "))}</TableRowColumn>
                       <TableRowColumn>
                         <RaisedButton primary={true} icon={<AddIcon/>}/>
                       </TableRowColumn>
                     </TableRow>
+                  ))
+               }
               </TableBody>
           </Table>
           </Scrollbars>
@@ -97,4 +80,16 @@ ApplicantRecommendations.styles = {
     },
 };
 
-export default ApplicantRecommendations;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    courses: state.course
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadCourses: (curr) => dispatch(courseActions.loadCourses(curr))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ApplicantRecommendations);
