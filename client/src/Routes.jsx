ea4125ee14fd05f91c5df1c803c1c84b1ae1bcc1
Andrew Xia <andrew.xia@live.ca>
2017-04-06 22:18:11 -0400

    finished login page resolved #90

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/App.jsx';
+import Login from './containers/login/login.jsx';
 import Main from './containers/main/Main.jsx';
 
 export default(
   <Route path='/' component={App}>
-    <IndexRoute component={Main} />
+    <IndexRoute component={Login} />
+    <Route path="/main" component={Main}/>
   </Route>
 );
